9371b32f9c5e0af5df86fc40bb63750169782d19
Alejandro <aml8912@gmail.com>
2023-08-23 09:13:21 -0500

    check db connection

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,7 +2,7 @@ import { DB_HOST, PORT, DB_NAME, DB_USER, DB_PASSWORD } from "./config";
 import { Sequelize } from "sequelize";
 
 const DATABASE_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_NAME}`;
-
+console.log(DATABASE_URL);
 const objectSequelize = {
   logging: false,
   native: false,
