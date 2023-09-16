ea9019ebbef75b1d1d42967682878e20fa340e13
Alejandro_Munoz <a.munoz254@uniandes.edu.co>
2023-09-16 15:07:41 -0500

    change from windows, deleted .env

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,3 +8,4 @@ export const PORT = Number(process.env.PORT);
 export const API_PORT = Number(process.env.API_PORT);
 export const CORS_URL = String(process.env.CORS_URL);
 export const GOOGLE_API_KEY = String(process.env.GOOGLE_API_KEY);
+//some stupid change
